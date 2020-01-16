14d04c52dd24be7b74434f283cc05457b4cb00ae
AdamJD01 <ajdennis1998@gmail.com>
2020-01-16 15:34:21 +0000

    Update

--- Variables.cs
+++ Variables.cs
@@ -5,44 +5,46 @@ namespace LearnProgrammingTool
 {
     public partial class frmVariables : Form
     {
-        private int score = 0;
-        //private int scoreDummy = 0;
+        private int scoreQuestion1;
+        private int scoreQuestion2;
+        private int finalScore;
 
         public frmVariables()
         {
             InitializeComponent();
             MaximizeBox = false; //stop the user making the form larger
-        }
+    }
 
         private void btnNextQuestion1_Click(object sender, EventArgs e)
         {
             //TO DO: For now this is hard coded but in future the options for the combo boxes will need to be connected to a database
 
             //validation
-            if (cmbQuestion1.Equals("") || cmbQuestion2.Equals(""))
+            if (cmbQuestion1.SelectedIndex < 0 || cmbQuestion2.SelectedIndex < 0)
             {
                 MessageBox.Show("Please choose an option");
-                txtScore.Text = score.ToString();
             }
 
             //calculation
             else if (cmbQuestion1.SelectedItem.Equals("String"))
             {
-                score += 1;
+                scoreQuestion1++;
             }
 
             else if (cmbQuestion2.SelectedItem.Equals("Boolean"))
             {
-                score += 1;
+                scoreQuestion2++;
             }
 
-            //else
-            //{
-            //   score += scoreDummy;
-            //}
+            finalScore = scoreQuestion1 + scoreQuestion2;
+
+            if(finalScore > 2)
+            {
+               finalScore = 2;
+            }
 
             //output
-            txtScore.Text = score.ToString();
+            txtScore.Text = finalScore.ToString();
         }
     }
 }
