df4dafdcf434befa50b8881a6da16f68491c4953
AdamJD01 <ajdennis1998@gmail.com>
2020-01-28 11:51:09 +0000

    Update

--- Variables.cs
+++ Variables.cs
@@ -5,11 +5,9 @@ namespace LearnProgrammingTool
 {
     public partial class frmVariables : Form
     {
-        private int scoreQuestion1;
-        private int scoreQuestion2;
+        private int scoreAnswer1;
+        private int scoreAnswer2;
         private int finalScore;
-        private string answer1;
-        private string answer2;
 
         public frmVariables()
         {
@@ -21,37 +19,34 @@ namespace LearnProgrammingTool
         {
             //TO DO: For now this is hard coded but in future the options for the combo boxes will need to be connected to a database
 
-            answer1 = "String";
-            answer2 = "Boolean";
-
             //validation
             if (cmbQuestion1.SelectedIndex < 0 || cmbQuestion2.SelectedIndex < 0)
             {
                 MessageBox.Show("Please choose an option");
+                scoreAnswer1 = 0;
+                scoreAnswer2 = 0;
             }
 
             //calculation
-            else if (cmbQuestion1.SelectedItem.Equals("String"))
+            else if (cmbQuestion1.SelectedItem.Equals("String") && cmbQuestion2.SelectedItem.Equals("Boolean"))
             {
-                scoreQuestion1 = 1;
+                scoreAnswer1 = 1;
+                scoreAnswer2 = 1;
             }
 
-            else if (cmbQuestion1.Text != answer1)
+            else if (cmbQuestion1.SelectedItem.Equals("String") || cmbQuestion2.SelectedItem.Equals("Boolean"))
             {
-                scoreQuestion1 = 0;
+                scoreAnswer1 = 1;
+                scoreAnswer2 = 0;
             }
 
-            else if (cmbQuestion2.SelectedItem.Equals("Boolean"))
+            else 
             {
-                scoreQuestion2 = 1;
+                scoreAnswer1 = 0;
+                scoreAnswer2 = 0;
             }
 
-            else if (cmbQuestion2.Text != answer2)
-            {
-                scoreQuestion2 = 0;
-            }
-
-            finalScore = scoreQuestion1 + scoreQuestion2;
+            finalScore = scoreAnswer1 + scoreAnswer2;
 
             //output
             txtScore.Text = finalScore.ToString();
